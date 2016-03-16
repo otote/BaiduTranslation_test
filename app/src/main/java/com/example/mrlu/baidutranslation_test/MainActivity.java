3e1d36e85e3b289f9b9cff8d6c86a30de2819b5e
Mr Lu <lxxlxx888@163.com>
2016-03-16 22:44:42 +0800

    update about  xml

--- app/src/main/java/com/example/mrlu/baidutranslation_test/MainActivity.java
+++ app/src/main/java/com/example/mrlu/baidutranslation_test/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.mrlu.baidutranslation_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_input;
+    TextView tv_translate;
+
+    //百度api的相关信息
+    private final  String appId = "20160316000015704";
+    private final  String password = "o8X_F27SJAN6a4kAFZGA";
+    private final  String request = "http://api.fanyi.baidu.com/api/trans/vip/translate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
